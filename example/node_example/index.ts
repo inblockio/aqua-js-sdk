dfd5c61a4f5874769d0bd1ebef301b7cfa9a9d4f
Dalmas Ogembo <dalmasogembo@gmail.com>
2025-02-07 13:29:00 +0300

    Setup testing env

--- example/node_example/index.ts
+++ example/node_example/index.ts
@@ -1,6 +1,8 @@
 
 import * as fs from "fs"
 
+import AquaTree, { AquaObject, FileObject } from "aqua-protocol"
+
 function readAquaFile(aquaFilePath: string): AquaObject | null {
     try {
         // Read the file synchronously
@@ -17,3 +19,33 @@ function readAquaFile(aquaFilePath: string): AquaObject | null {
         return null; // Return null or throw an error, depending on your use case
     }
 }
+
+
+function readFile(aquaFilePath: string): string | null {
+    try {
+        // Read the file synchronously
+        const fileContent = fs.readFileSync(aquaFilePath, { encoding: "utf-8" });
+
+        // Return the parsed object
+        return fileContent;
+    } catch (error) {
+        // Handle errors (e.g., file not found, invalid JSON)
+        console.error(`Error reading or parsing the file at ${aquaFilePath}:`, error);
+        return null; // Return null or throw an error, depending on your use case
+    }
+}
+
+let aquaTree = new AquaTree()
+
+let aquaObject = readAquaFile("./README.md.aqua.json")
+let testFileContent = readFile("./test.txt")
+
+let aquaFileObject: FileObject = {
+    fileName: "text.txt",
+    fileContent: testFileContent ?? "",
+    path: "./text.txt"
+}
+
+let newAquaObject = aquaTree.createGenesisRevision(aquaFileObject)
+
+console.log(newAquaObject)
