c0a8c75b066555aa81f5744541a800e15e99056e
Dalmas Ogembo <dalmasogembo@gmail.com>
2025-02-13 17:05:53 +0300

    Update test func to write to file

--- example/node_example/index.ts
+++ example/node_example/index.ts
@@ -26,6 +26,21 @@ function readAquaFile(aquaFilePath: string): AquaTree | null {
 }
 
 
+function writeAquaFile(aquaFilePath: string, aquaTree: AquaTree): AquaTree | null {
+    try {
+        // Read the file synchronously
+        const fileContent = fs.writeFileSync(aquaFilePath, JSON.stringify(aquaTree, null, 4), { encoding: "utf-8" });
+
+        // Return the parsed object
+        return aquaTree;
+    } catch (error) {
+        // Handle errors (e.g., file not found, invalid JSON)
+        console.error(`Error reading or parsing the file at ${aquaFilePath}:`, error);
+        return null; // Return null or throw an error, depending on your use case
+    }
+}
+
+
 function readFile(aquaFilePath: string): string | null {
     try {
         // Read the file synchronously
@@ -61,17 +76,16 @@ let creds = {
     "witness_eth_platform": "metamask"
 }
 
-// let newAquaTree = aquafier.createGenesisRevision(aquaFileObject)
 
 async function chainExample() {
 
     const result = await new AquafierChainable(null).notarize(aquaFileObject)
-        .then(chain => chain.sign("cli", creds))
-        .then(chain => chain.witness('eth', 'sepolia', 'cli', creds))
+        .then(chain => chain.sign())
+        .then(chain => chain.witness())
         .then(chain => chain.verify([aquaFileObject]))
         .then(chain => {
-            // printLogs(chain.getLogs())
-            // console.log(JSON.stringify(chain.getValue(), null, 4))
+            let aquaTree = chain.getValue()
+            writeAquaFile('test.aqua.json', aquaTree)
             return chain
         })
         .then(chain => chain.getVerificationValue())
@@ -83,10 +97,36 @@ async function chainExample() {
         printLogs(result.data.logData, true)
     }
     else {
-        // console.table(result)
-        // printLogs(result, true)
         console.log("Some weird error occured!")
     }
 };
 
+async function chainExampleWithMultipleParameters() {
+
+    const result = await new AquafierChainable(null).notarize(aquaFileObject)
+        .then(chain => chain.sign("cli", creds))
+        .then(chain => chain.sign("metamask", creds))
+        .then(chain => chain.witness('eth', 'sepolia', 'cli', creds))
+        .then(chain => chain.witness('eth', 'sepolia', 'metamask', creds))
+        .then(chain => chain.witness('eth', 'holesky', 'metamask', creds))
+        .then(chain => chain.verify([aquaFileObject]))
+        .then(chain => {
+            let aquaTree = chain.getValue()
+            writeAquaFile('test_complete.aqua.json', aquaTree)
+            return chain
+        })
+        .then(chain => chain.getVerificationValue())
+        .catch(error => console.log(`An error occured ${error}`))
+
+    console.log("================= Result ==================")
+    console.log(JSON.stringify(result, null, 4))
+    if (result!.isOk()) {
+        printLogs(result.data.logData, true)
+    }
+    else {
+        console.log("Some weird error occured!")
+    }
+};
+
+chainExampleWithMultipleParameters()
 chainExample();
