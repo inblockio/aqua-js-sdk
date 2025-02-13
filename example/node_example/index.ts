680673c2f2bcad8c719943bd79f502edc28b06f8
Dalmas Ogembo <dalmasogembo@gmail.com>
2025-02-13 13:36:12 +0300

    Add chainnable class

--- example/node_example/index.ts
+++ example/node_example/index.ts
@@ -5,6 +5,52 @@ import * as fs from "fs"
 import Aquafier, { AquaTree, FileObject, notarize } from "aquafier-js-sdk"
 import { sign } from "crypto";
 
+
+export class Chainable {
+    private value: AquaTree;
+
+    constructor(initialValue: AquaTree) {
+        this.value = initialValue;
+    }
+
+    notarize(): this {
+        this.value += n;
+        return this;
+    }
+
+    sign(n: number): this {
+        this.value -= n;
+        return this;
+    }
+
+    witness(n: number): this {
+        this.value *= n;
+        return this;
+    }
+
+    content(n: number): this {
+        this.value *= n;
+        return this;
+    }
+
+    async link(n: number): Promise<this> {
+        if (n === 0) throw new Error("Cannot divide by zero");
+        this.value /= n;
+        return this;
+    }
+
+    async form(n: number): Promise<this> {
+        await new Promise(resolve => setTimeout(resolve, 100)); // Simulating async operation
+        this.value += n;
+        return this;
+    }
+
+    getValue(): number {
+        return this.value;
+    }
+}
+
+
 function readAquaFile(aquaFilePath: string): AquaTree | null {
     try {
         // Read the file synchronously
@@ -51,9 +97,29 @@ let aquaFileObject: FileObject = {
 
 // let newAquaTree = aquafier.createGenesisRevision(aquaFileObject)
 
-let newAquaTree = notarize(aquaFileObject).then((val) => sign(val));
+// let newAquaTree = notarize(aquaFileObject).then((val) => sign(val));
 
 // console.log(newAquaTree)
+
+
+async function chainnableCall() {
+    const result = await new Chainable(10)
+        .add(5)
+        .subtract(2)
+        .multiply(3)
+        .divide(2);
+
+    console.log(result.getValue()); // Outputs: 19.5
+
+    const asyncResult = await new Chainable(10)
+        .add(5)
+        .asyncAdd(5)
+        .then(instance => instance.multiply(2));
+
+    console.log(asyncResult.getValue()); // Outputs: 40
+}
+
+chainnableCall()
 
 
 
