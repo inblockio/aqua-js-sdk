1e4986944a2f1ccaf695145021bc8d496caf8c0f
Dalmas Ogembo <dalmasogembo@gmail.com>
2025-02-19 16:08:16 +0300

    UPdate test function on forms example

--- example/forms_example/src/App.tsx
+++ example/forms_example/src/App.tsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
+import Aquafier, { FileObject } from "aquafier-js-sdk";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const tryAquafier = async () => {
+    let aquafier = new Aquafier();
+    const fileObject: FileObject = {
+      fileName: "test.txt",
+      fileContent: "Sample content",
+      path: "/fake/path/test.txt"
+    };
+
+    const result = await aquafier.createGenesisRevision(fileObject);
+    console.log("Result")
+    console.log(JSON.stringify(result, null, 4))
+
+  }
+
+  useEffect(() => {
+    tryAquafier()
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      tryAquafier
     </>
   )
 }
