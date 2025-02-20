6e83d443f9b7f71c04098efb3ca8ce4d595f90ef
arthur-kamau <kamaukenn11@gmail.com>
2025-02-20 14:13:46 +0300

    fixes

--- example/browser/src/App.tsx
+++ example/browser/src/App.tsx
@@ -1,30 +1,32 @@
 import { useEffect } from 'react'
 import Aquafier, { FileObject } from "aquafier-js-sdk";
+import './App.css'
 
 function App() {
 
-  // const [files, setFiles] = useState<FileObject[]>([]);
+  const tryAquafier = async () => {
+    let aquafier = new Aquafier();
+    const fileObject: FileObject = {
+      fileName: "test.txt",
+      fileContent: "Sample content",
+      path: "/fake/path/test.txt"
+    };
+
+    const result = await aquafier.createGenesisRevision(fileObject);
+    console.log("Result")
+    console.log(JSON.stringify(result, null, 4))
+
+  }
 
   useEffect(() => {
-    (async () => {
-      let aquafier = new Aquafier();
-      const fileObject: FileObject = {
-        fileName: "test.txt",
-        fileContent: "Sample content",
-        path: "/fake/path/test.txt"
-      };
-
-      const result = await aquafier.createGenesisRevision(fileObject);
-      console.log("Result")
-      console.log(JSON.stringify(result, null, 4))
-    })()
-
+    tryAquafier()
   }, [])
 
-
   return (
-   <div>Check browser console for the genesis aqua tree object.</div>
-  );
+    <>
+      tryAquafier
+    </>
+  )
 }
 
 export default App
